f4fbdc897bff9ca991aa34df8f082d013b37978e
juanpcruz0209 <jcruzgu@unal.edu.co>
2022-09-13 14:44:10 +0000

    Avance hasta el punto 9

---
+++ binder/CruzJuanPablo_S5C2_repasoC.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+	int A=7;
+	float B=5.1;
+	cout << "La primera tiene un valor de ";
+	cout << A << "\n";
+        cout << "y la segunda variable tiene un valor de ";
+	cout << B << "\n";
+	float C=A/B;
+	cout << "El resultado es ";
+	cout << C << "\n";
+    int D[300]={}
+    for ( int i = 0; i < 301; i++) {
+        D[i] = rand() % 900;
+        cout << D[i] << " ";
+    }
+	return 0;
+}
